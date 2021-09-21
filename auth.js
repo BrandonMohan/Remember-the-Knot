e90fea83105881a88f7fdf3928ea1ea8cf7859f6
Howard Chang <changh341@gmail.com>
2021-09-20 18:01:14 -0700

    authentication issue fixed, can now login and access homepage

--- auth.js
+++ auth.js
@@ -4,7 +4,6 @@ const loginUser = (req, res, user) => {
     req.session.auth = {
         userId: user.id,
     };
-    // console.log(req.session);
 };
 
 const logoutUser = (req, res) => {
@@ -14,12 +13,13 @@ const logoutUser = (req, res) => {
 };
 
 
-// const requireAuth = (req, res, next) => {
-//     if (!res.locals.authenticated) {
-//       return res.redirect('/users/login');
-//     }
-//     return next();
-//   };
+const requireAuth = (req, res, next) => {
+    if (!res.locals.authenticated) {
+        console.log('Login in please')
+        return res.redirect('/login');
+    }
+    return next();
+};
 
 const restoreUser = async (req, res, next) => {
     // Log the session object to the console
@@ -27,28 +27,29 @@ const restoreUser = async (req, res, next) => {
     console.log(req.session);
 
     if (req.session.auth) {
-      const { userId } = req.session.auth;
+        const { userId } = req.session.auth;
 
-      try {
-        const user = await db.User.findByPk(userId);
+        try {
+            const user = await db.User.findByPk(userId);
 
-        if (user) {
-          res.locals.authenticated = true;
-          res.locals.user = user;
-          next();
+            if (user) {
+                res.locals.authenticated = true;
+                res.locals.user = user;
+                next();
+            }
+        } catch (err) {
+            res.locals.authenticated = false;
+            next(err);
         }
-      } catch (err) {
-        res.locals.authenticated = false;
-        next(err);
-      }
     } else {
-      res.locals.authenticated = false;
-      next();
+        res.locals.authenticated = false;
+        next();
     }
-  };
+};
 
 module.exports = {
     loginUser,
     logoutUser,
     restoreUser,
+    requireAuth
 }
