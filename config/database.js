efa1748b32883f78ae88e5dad3303a4f657c7825
BrandonMohan <barndonmo@gmail.com>
2021-09-22 18:35:37 -0400

    updated heroku database url

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: 'postgres://iwdofmybelexot:b2df3bf4c2d24ee8f4685c4a57f5afcbee5dc2d5231e3d684cca4a8c6e93e6fa@ec2-54-145-110-118.compute-1.amazonaws.com:5432/ddq2l5igakopel',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
