2914cb9925acf6d6002b34c40dc7da4483f251b6
Howard Chang <changh341@gmail.com>
2021-09-23 02:12:07 -0700

    task can be created via DOM

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -1,141 +1,141 @@
 
 document.addEventListener('DOMContentLoaded', async () => {
-  //GET LISTS
-  const listcontainer = document.querySelector('.list_container');
-  const result = await fetch('/app/lists');
-  const { lists } = await result.json();
-  let listname = {};
-  for (let list of lists) {
-    const { listName, id } = list;
-    listname[id] = listName;
-  }
-  let listHtml = [];
-  for (let key in listname) {
-    listHtml.push(
-      `<div data-id="${key}" class="list_div">${listname[key]}</div>`
-    );
-  }
-  listcontainer.innerHTML = listHtml.join('');
 
-  // GET TASKS ONTO LISTS
-  let firstList = Object.keys(listname)[0]; // default view is first list
-  const listHeader = document.getElementById('listTitle')
-  listHeader.innerHTML = `<h2 id=header  data-id="${firstList}"> ${Object.values(listname)[0]}<h2>`
-  const getTasks = await fetch(`/app/lists/${firstList}/tasks`);
-  const { tasks } = await getTasks.json();
-  let taskObj = {};
-  let taskHtml = [];
-  for (let task of tasks) {
-    const { taskName, id } = task;
-    taskObj[id] = taskName;
-  }
-  for (let task in taskObj) {
-    taskHtml.push(`<li data-id="${task}" class="task_li">${taskObj[task]}</li>`);
-  }
-  const taskList = document.querySelector('.task_list');
-  taskList.innerHTML = taskHtml.join('');
-
-
-  const listDiv = document.getElementsByClassName("list_div")
-  for (let div of listDiv) {
-    div.addEventListener('click', async () => {
-      const id = div.dataset.id
-      const newListTitle = div.innerText
-      const getTasks = await fetch(`/app/lists/${id}/tasks`);
-      const { tasks } = await getTasks.json();
-
-      let taskObj = {};
-      let taskHtml = [];
+  const fetchList = async () => {
+    const reply = await fetch('/app/lists');
+    const { lists } = await reply.json();
+    return lists
+  };
+  const fetchTasks = async (listId) => {
+    const reply = await fetch(`/app/lists/${listId}/tasks`)
+    const { tasks } = await reply.json()
+    return tasks
+  };
+  const renderList = (lists) => {
+    const listcontainer = document.querySelector('.list_container');
+    let listHtml = [];
+    for (let list of lists) {
+      const { id, listName } = list
+      listHtml.push(
+        `<div data-listId="${id}" class="list_div">${listName}</div>`
+      );
+    }
+    return listcontainer.innerHTML = listHtml.join('');
+  };
+  const renderTasks = (tasks) => {
+    const taskList = document.querySelector('.task_list');
+    let taskHtml = [];
+    if (tasks.length) {
       for (let task of tasks) {
         const { taskName, id } = task;
-        taskObj[id] = taskName;
+        taskHtml.push(`<li data-taskId="${id}" class="task_li">${taskName}</li>`);
       }
-      for (let task in taskObj) {
-        taskHtml.push(`<li>${taskObj[task]}</li>`);
-      }
-      taskList.innerHTML = taskHtml.join('');
-      listHeader.innerHTML = `<h2 id=header data-id="${id}">${newListTitle}</h2>`
-      console.log(id)
+    }
+    return taskList.innerHTML = taskHtml.join('');
+  };
+  const addList = async (listName) => {
+    const listcontainer = document.querySelector('.list_container');
+    const res = await fetch('/app/lists', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({ listName })
+    })
+    return await res.json();
+  }
+  const addTask = async (listId, taskName) => {
+    const res = await fetch(`/app/lists/${listId}/tasks`, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({ taskName })
+    })
+    return await res.json()
+  };
+  const addTaskEventHandler = () => {
 
-      newTaskButton.addEventListener('click', (e) => {
-        newTaskButton.setAttribute('type', 'hidden')
-        newTaskInput.setAttribute('type', 'text')
-        newTaskInput.focus()
-        newTaskInput.addEventListener('focusout', (e) => {
-          newTaskInput.setAttribute('type', 'hidden')
-          newTaskInput.value = ''
-          newTaskButton.setAttribute('type', 'button')
-        })
-        newTaskInput.addEventListener("keyup", async (e) => {
-          console.log(id)
-          if (e.key === 'Enter') {
-            const result = await fetch(`/app/lists/${id}/tasks`, {
-              method: 'POST',
-              headers: { 'Content-type': 'application/json' },
-              body: JSON.stringify({ taskName: `${newTaskInput.value}` })
-            })
-            if (result) {
-              newTaskInput.value = ''
-              const { id, taskName } = await result.json();
-              let newElement = document.createElement('li')
-              newElement.innerText = `${taskName} `
-              taskList.appendChild(newElement)
-
-            }
-          }
-        })
+    const taskList = document.querySelector(".task_list")
+    newTaskButton.addEventListener('click', (e) => {
+      newTaskButton.setAttribute('type', 'hidden')
+      newTaskInput.setAttribute('type', 'text')
+      newTaskInput.focus()
+      newTaskInput.addEventListener('focusout', (e) => {
+        newTaskInput.setAttribute('type', 'hidden')
+        newTaskInput.value = ''
+        newTaskButton.setAttribute('type', 'button')
+      })
+      newTaskInput.addEventListener("keyup", async (e) => {
+        e.stopImmediatePropagation()
+        if (e.key === 'Enter') {
+          const task = await addTask(lastClickedListId, newTaskInput.value)
+          if (task) {
+            newTaskInput.value = ''
+            const { id, taskName } = task;
+            let newElement = document.createElement('li')
+            newElement.innerText = `${taskName} `
+            taskList.appendChild(newElement)
+          };
+        }
       })
     })
   }
-
-  const newListButton = document.getElementById("newListButton")
-  const newListInput = document.getElementById("newListInput")
-  newListButton.addEventListener('click', (e) => {
-    newListButton.setAttribute('type', 'hidden')
-    newListInput.setAttribute('type', 'text')
-    newListInput.focus()
-    newListInput.addEventListener('focusout', (e) => {
-      newListInput.setAttribute('type', 'hidden')
-      newListInput.value = ''
-      newListButton.setAttribute('type', 'button')
-    })
-    newListInput.addEventListener("keyup", async (e) => {
-      if (e.key === 'Enter') {
-        e.stopImmediatePropagation();
-        const result = await fetch('/app/lists', {
-          method: 'POST',
-          headers: { 'Content-type': 'application/json' },
-          body: JSON.stringify({ listName: `${newListInput.value} ` })
-        })
-        if (result) {
-          newListInput.value = ''
-          const { id, listName } = await result.json();
-          let newList = document.createElement('div')
-          newList.className = 'list_div'
-          newList.innerText = `${listName} `
-          newList.addEventListener('click', async () => {
-            const getTasks = await fetch(`/ app / lists / ${id} /tasks`);
-            const { tasks } = await getTasks.json();
-            let taskObj = {};
-            let taskHtml = [];
-            for (let task of tasks) {
-              const { taskName, id } = task;
-              taskObj[id] = taskName;
-            }
-            for (let task in taskObj) {
-              taskHtml.push(`<li>${taskObj[task]}</li>`);
-            }
-            taskList.innerHTML = taskHtml.join('');
-          })
-          listcontainer.appendChild(newList)
+  const addListEventHandler = () => {
+    const listcontainer = document.querySelector('.list_container');
+    const newListButton = document.getElementById("newListButton")
+    const newListInput = document.getElementById("newListInput")
+    newListButton.addEventListener('click', (e) => {
+      newListButton.setAttribute('type', 'hidden')
+      newListInput.setAttribute('type', 'text')
+      newListInput.focus()
+      newListInput.addEventListener('focusout', (e) => {
+        newListInput.setAttribute('type', 'hidden')
+        newListInput.value = ''
+        newListButton.setAttribute('type', 'button')
+      })
+      newListInput.addEventListener("keyup", async (e) => {
+        if (e.key === 'Enter') {
+          e.stopImmediatePropagation();
+          const res = await addList(newListInput.value)
+          if (res) {
+            newListInput.value = ''
+            const { id, listName } = res
+            let newList = document.createElement('div')
+            newList.className = 'list_div'
+            newList.innerText = `${listName} `
+            newList.addEventListener('click', async () => {
+              renderTasks([])
+            })
+            listcontainer.appendChild(newList)
+          };
         }
-      }
+      })
     })
-  })
+  }
+  const fetchListEventHandler = async () => {
+    const listDiv = document.querySelector(".list_container")
+    listDiv.addEventListener('click', async (event) => {
+      const listId = event.target.dataset.listid;
+      if (listId) {
+        const tasks = await fetchTasks(listId)
+        renderTasks(tasks)
+        lastClickedListId = Number(listId);
+      }
+    });
+  }
 
 
+  const lists = await fetchList();
+  renderList(lists);
 
+  let lastClickedListId;
 
+  if (lists[0].id) {
+    const tasks = await fetchTasks(lists[0].id);
+    renderTasks(tasks);
+    lastClickedListId = lists[0].id;
+  }
+
+  addListEventHandler();
+  addTaskEventHandler();
+  fetchListEventHandler();
 
 
 
